4b57ed7ce0d3cff020898e72454daaa4d65212f5
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-04-24 15:27:40 -0300

    Salvando material

--- src/Snake.java
+++ src/Snake.java
@@ -9,11 +9,10 @@ public class Snake extends JFrame implements Runnable{
 	private int posTopMaca = 200;
 	private byte pos = 1;
 	private Thread th = new Thread(this);
-	private static int placar = 0;
-	private JButton btReiniciar = new JButton("Reiniciar");
+	private int scoreboard = 0;
 	
 	public Snake() {
-		super("Cobrinha - Placar" + placar);
+		this.setTitle("Snake - Scoreboard" + scoreboard);
 		this.getContentPane().setBackground(Color.GREEN);
 		this.setSize(500,500);
 		//this.setResizable(false);
@@ -22,29 +21,31 @@ public class Snake extends JFrame implements Runnable{
 			public void keyPressed(KeyEvent e){
 				// System.out.println(e.getKeyCode()); //pegar tecla apertada e exibir no console.
 				switch(e.getKeyCode()){
-				case 37:
-					pos = -1;break;
-				case 38:
-					pos = 2;break;
-				case 39:
-					pos = 1;break;
-				case 40:
-					pos = -2;break;
+					case 37:
+						pos = -1;break;
+					case 38:
+						pos = 2;break;
+					case 39:
+						pos = 1;break;
+					case 40:
+						pos = -2;break;
+					}
 				}
-			}
-		});
-		btReiniciar.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				reiniciar();
-			}
-		});
-		this.add(btReiniciar, BorderLayout.NORTH);
+			});
 		this.repaint(); // prevenir bugs, se nao aparecer nada;
 		th.start();
 		this.setVisible(true);
 	}
 
-	public void reiniciar(){
+	public void retry(){
+		posEsqSnake = new int[]{ 100 };
+        posTopSnake = new int[]{ 100 };
+        posEsqMaca = 200;
+        posTopMaca = 200;
+        pos = 1;
+        th = new Thread(this);
+        scoreboard = 0;
+        this.setTitle("Snake - Scoreboard " + scoreboard);
 		th.start();
 	}
 	
@@ -53,9 +54,9 @@ public class Snake extends JFrame implements Runnable{
 				posTopSnake[0] > 30 && posTopSnake[0] < 500){
 			if(posEsqSnake[0] >= posEsqMaca && posEsqSnake[0] <= posEsqMaca + 20 &&
 					posTopSnake[0] >= posTopMaca && posTopSnake[0] <= posTopMaca + 20) {
-				crescerSnake();
-				placar++;
-				this.setTitle("Cobrinha - Placar" + placar);
+				growSnake();
+				scoreboard++;
+				this.setTitle("Snake - Scoreboard" + scoreboard);
 				posTopMaca = (int)(Math.random()* 400 + 50);
 				posEsqMaca = (int)(Math.random()* 450 + 10);
 				repaint();
@@ -78,9 +79,51 @@ public class Snake extends JFrame implements Runnable{
 			}catch (InterruptedException e ){
 			}
 		}
-		JOptionPane.showMessageDialog(this, "Terminou");
+		int reply = JOptionPane.showConfirmDialog(
+				this, "Deseja jogar novamente?", "Cobrinha", JOptionPane.YES_NO_OPTION);
+        if (reply == JOptionPane.YES_OPTION) {
+	            retry();
+        }else{
+            JOptionPane.showMessageDialog(this, "Terminou");
+           //System.exit(0);
+        }
 	}
 	
+	private void growSnake(){
+		int posEsqBkp [] = posEsqSnake; 
+		int posTopBkp [] = posTopSnake;
+		
+		posEsqSnake = new int[posEsqSnake.length + 1];
+		posTopSnake = new int[posTopSnake.length + 1];
+	
+		for(int i = 0; i < posEsqBkp.length; i++){
+			posEsqSnake[i] = posEsqBkp[i];
+		}
+		for(int i = 0; i < posTopBkp.length; i++){
+			posTopSnake[i] = posTopBkp[i];
+		}
+		posEsqSnake[posEsqSnake.length-1] = posEsqSnake[posEsqSnake.length-2];
+		posEsqSnake[posEsqSnake.length-1] = posEsqSnake[posEsqSnake.length-2];
+		walk();
+	}
+	
+	private void walk(){
+		for(int i = posEsqSnake.length - 1; i > 0; i--){
+			posEsqSnake[i] = posEsqSnake[i-1];
+		}
+		for(int i = posTopSnake.length - 1; i > 0; i--){
+			posTopSnake[i] = posTopSnake[i-1];
+		}
+		if(posEsqSnake.length > 1){
+			switch(pos){
+				case 1: posEsqSnake[0] = posEsqSnake[1] + 20;break;
+				case -1:posEsqSnake[0] = posEsqSnake[1] - 20;break;
+				case 2: posTopSnake[0] = posTopSnake[1] + 20;break;
+				case -2:posTopSnake[0] = posTopSnake[1] - 20;break;
+			}
+		}
+	}
+		
 	public void paint(Graphics g) {
 		super.paint(g);
 		g.setColor(Color.WHITE);
@@ -90,42 +133,8 @@ public class Snake extends JFrame implements Runnable{
 		g.setColor(Color.RED);
 		g.fillOval(posEsqMaca,posTopMaca,20,20);
 	}
-	
+		
 	public static void main(String[] args) {
 		new Snake();
-	}
-	
-	private void crescerSnake(){
-		int posEsqBkp [] = posEsqSnake; 
-		int posTopBkp [] = posTopSnake;
-		
-		posEsqSnake = new int[posEsqSnake.length + 1];
-		posTopSnake = new int[posTopSnake.length + 1];
-	
-		for(int i = 0; i > posEsqBkp.length; i++){
-			posEsqSnake[i] = posEsqBkp[i];
-		}
-		for(int i = 0; i > posTopBkp.length; i++){
-			posTopSnake[i] = posTopBkp[i];
-		}
-		andar();
-	}
-		private void andar(){
-			for(int i = posEsqSnake.length - 1; i > 0; i--){
-				posEsqSnake[i] = posEsqSnake[i-1];
-			}
-			for(int i = posTopSnake.length - 1; i > 0; i--){
-				posTopSnake[i] = posTopSnake[i-1];
-			}
-			if(posEsqSnake.length > 1){
-			switch(pos){
-			case 1: posEsqSnake[0] = posEsqSnake[1] + 20;break;
-			case -1:posEsqSnake[0] = posEsqSnake[1] - 20;break;
-			case 2: posTopSnake[0] = posTopSnake[1] + 20;break;
-			case -2:posTopSnake[0] = posTopSnake[1] - 20;break;
-			}
-		}
-		System.out.println(posEsqSnake);
-		System.out.println(posTopSnake);
 	}
 }
